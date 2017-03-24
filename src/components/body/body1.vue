9ac6f596c863d3dbb965f49ecb2c783aa36715f0
YOU <youwg@suninfo.com>
2017-03-24 18:14:41 +0800

    step complete

--- src/components/body/body1.vue
+++ src/components/body/body1.vue
@@ -1,16 +1,23 @@
 <template>
   <div class="body">
     <s-sub-nav :list="list"></s-sub-nav>
+    <f-box title="这是一个示例" :bars="[1,2,3]" width="600px" height="500px">
+      <div slot="0">123</div>
+      <div slot="1">123</div>
+      <div slot="2">123</div>
+    </f-box>
     <router-view></router-view>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
+  import box from '@/components/f-box/f-box';
   import sub from '@/components/sub-nav/sub-nav';
   export default {
     name: 'body2',
     components: {
-      's-sub-nav': sub
+      's-sub-nav': sub,
+      'f-box': box
     },
     data () {
       return {
@@ -31,4 +38,6 @@
 
 <style lang="stylus" rel="stylesheet/stylus">
   @import "body.styl"
+  .f-box
+    margin: 40px
 </style>
